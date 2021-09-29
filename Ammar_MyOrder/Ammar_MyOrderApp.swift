ff3184c146ee5c221c2e4ea1dbef5bb6f85b8d57
sahil2016sam <ammarummar@hotmail.com>
2021-09-29 01:16:19 -0400

    Initial Commit

---
+++ Ammar_MyOrder/Ammar_MyOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ammar_MyOrderApp.swift
+//  Ammar_MyOrder
+//
+//  Created by Ammar Khan on 2021-09-29.
+//
+
+import SwiftUI
+
+@main
+struct Ammar_MyOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
